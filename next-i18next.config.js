247799da10c83c5aa8a0648d739ec11bdd3dd845
Hoshi Ran <ureshinostyle350@gmail.com>
2022-05-15 18:11:26 +0900

    add: next-i18next config

---
+++ next-i18next.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('next-i18next').UserConfig}
+ */
+module.exports = {
+  i18n: {
+    defaultLocale: process.env.NEXT_PUBLIC_DEFAULT_LOCALE || "ja",
+    locales: (
+      process.env.NEXT_PUBLIC_SUPPORTED_LOCALES_SEPARATE_BY_COMMA || "ja"
+    ).split(","),
+  },
+  defaultNS: "common",
+  keySeparator: ".",
+  reloadOnPrerender: true,
+};
